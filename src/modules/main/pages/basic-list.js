d8e83272a69d57f8c5aa07795fb5bdd68ab6419a
Humiao <1124003166@qq.com>
2020-10-26 17:05:23 +0800

    修改基本列表界面数据展示

--- src/modules/main/pages/basic-list.js
+++ src/modules/main/pages/basic-list.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Table } from 'antd';
+import { Card, Table, Tag } from 'antd';
 
 import "../style/basic-list.css"
 
@@ -31,7 +31,22 @@ class BasicList extends Component {
                     dataIndex: 'title',
                     key: 'title',
                     align: "center",
-                    width: 140
+                    width: 140,
+                    render: title => {
+                        switch (title) {
+                            case "钻石王老五":
+                                return (<Tag color='geekblue' key={title}>{title}</Tag>);
+                            case "无名鼠辈":
+                                return (<Tag color='green' key={title}>{title}</Tag>);
+                            case "蓝胖子":
+                                return (<Tag color='blue' key={title}>{title}</Tag>);
+                            case "海贼王":
+                                return (<Tag color='orange' key={title}>{title}</Tag>);
+                            default:
+                                return (<Tag color='volcano' key={title}>{title}</Tag>);
+
+                        }
+                    },
                 }, {
                     title: '爱好',
                     dataIndex: 'favorite',
@@ -60,32 +75,50 @@ class BasicList extends Component {
             ],
             dataSource: [
                 {
-                    id: "0",
-                    name: 'Tom',
+                    id: "1",
+                    name: '汤姆',
                     sex: "男",
                     title: "钻石王老五",
                     favorite: "抓老鼠",
                     birthday: "1990-01-02",
-                    address: "太阳系火星",
+                    address: "New York No. 1 Lake Park",
                     clockOn: "09:00"
                 }, {
-                    id: "1",
-                    name: 'Jarry',
+                    id: "2",
+                    name: '杰瑞',
                     sex: "男",
                     title: "无名鼠辈",
                     favorite: "偷奶酪",
                     birthday: "1999-01-01",
-                    address: "太阳系火星",
+                    address: "London No. 1 Lake Park",
                     clockOn: "01:00"
                 }, {
-                    id: "2",
-                    name: 'Spike',
+                    id: "3",
+                    name: '斯帕克',
                     sex: "男",
                     title: "忠犬八公",
                     favorite: "啃骨头",
                     birthday: "1995-01-01",
-                    address: "太阳系火星",
+                    address: "Sidney No. 1 Lake Park",
                     clockOn: "06:00"
+                }, {
+                    id: "4",
+                    name: '多啦A梦',
+                    sex: "男",
+                    title: "蓝胖子",
+                    favorite: "吃铜锣烧",
+                    birthday: "1998-01-01",
+                    address: "Japan No. 1 Lake Park",
+                    clockOn: "12:00"
+                }, {
+                    id: "5",
+                    name: '路飞',
+                    sex: "男",
+                    title: "海贼王",
+                    favorite: "吃肉",
+                    birthday: "1993-01-01",
+                    address: "Japan No. 1 Lake Park",
+                    clockOn: "18:00"
                 }
             ]
         }
@@ -94,7 +127,7 @@ class BasicList extends Component {
         return (
             <div className="basic-list-page">
                 <Card size="small" title="基础表格" style={{ marginBottom: "20px" }} headStyle={{ textAlign: "left", fontWeight: "bold", background: "#f0f2f5" }} bodyStyle={{ textAlign: "left" }}>
-                    <Table bordered dataSource={this.state.dataSource} columns={this.state.columns} />
+                    <Table bordered dataSource={this.state.dataSource} columns={this.state.columns} pagination={false} />
                 </Card>
             </div>
         );
